5eb6d494fbefd3309b87ba16427297a2a3435406
ASN <earth_to_us@naver.com>
2024-01-09 16:49:52 +0900

    #0109 추가 / 자료구조, ArrayResize 과제, +a

--- Homework/240109_ArrayResize/240109_ArrayResize_풀.cpp
+++ Homework/240109_ArrayResize/240109_ArrayResize_풀.cpp
@@ -1,24 +1,11 @@
-﻿
-#include <iostream>
-#include <Windows.h>
+﻿#include <iostream>
 #include <ConsoleEngine/EngineDebug.h>
-
-//_CrtSetDbgFlag(_CRTDBG_LEAK_CHECK_DF | _CRTDBG_ALLOC_MEM_DF);
-
 
 
 class IntArray
-
 {
-    
     // private: 디폴트 접근제한 지정자
 public:
-
-    IntArray()
-    {
-
-    }
-
     // 디폴트 생성자
     IntArray(int _Size)
     {
@@ -73,8 +60,6 @@ public:
         }
     }
 
-
-
     void ReSize(int _Size)
     {
         if (0 >= _Size)
@@ -82,32 +67,24 @@ public:
             MsgBoxAssert("배열의 크기가 0일수 없습니다");
         }
 
-        
+
         int* Ptr = ArrPtr;
-        // Ptr    = [0][1][2][3][4]
-        // ArrPtr = [?][?][?][?][?][?][?][?][?][?]
         ArrPtr = new int[_Size];
 
+        int CopySize = NumValue <= _Size ? NumValue : _Size;
 
-        Num();
-        for (int i = 0; i < Num(); i++)
+        for (int i = 0; i < CopySize; i++)
         {
-        ArrPtr[i] = Ptr[i];
-
+            ArrPtr[i] = Ptr[i];
         }
-
-       //==>  // ArrPtr = [0][1][2][3][4][?][?][?][?][?]
-
-
-
 
         NumValue = _Size;
 
-        if (nullptr != ArrPtr)
+        if (nullptr != Ptr)
         {
-            Release();
+            delete[] Ptr;
+            Ptr = nullptr;
         }
-
     }
 
     void Release()
@@ -127,16 +104,15 @@ private:
 
 int main()
 {
+    LeckCheck;
+
     IntArray NewArray = IntArray(5);
     // [?][?][?][?][?]
 
     for (int i = 0; i < NewArray.Num(); i++)
     {
-        //NewArray[i] = i;
-        
+        NewArray[i] = i;
     }
-
-   
     // [0][1][2][3][4]
 
     //for (size_t i = 0; i < NewArray.Num(); i++)
@@ -157,34 +133,22 @@ int main()
     // 2
     // 3
     // 4
-    // ?
-    // ?
-    // ?
-    // ?
-    // ?
+    // -134212312
+    // -134212312
+    // -134212312
+    // -134212312
+    // -134212312
 
- 
 
-     
 
     // 리사이즈 값은 얼마든지 변경될 수 있다.
     NewArray.ReSize(10);
     // ?는 무슨값이 들어가 있어도 괜찮다.
     // [0][1][2][3][4][?][?][?][?][?]
-    
-
-
-    
-
-
 
     for (size_t i = 0; i < NewArray.Num(); i++)
     {
         std::cout << NewArray[i] << std::endl;
     }
-
-
-
-
 
 }
